73dd3e3c298209190afb632c4eb3e98311579449
ismailelmogy <ismailelmogy0@gmail.com>
2022-05-26 05:56:19 +0200

    add font to app

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quotes/core/utils/app_colors.dart';
+import 'package:quotes/core/utils/app_strings.dart';
 
 ThemeData appTheme() {
   return ThemeData(
@@ -7,6 +8,7 @@ ThemeData appTheme() {
       hintColor: AppColors.hint,
       brightness: Brightness.light,
       scaffoldBackgroundColor: Colors.white,
+      fontFamily: AppStrings.fontFamily,
       textTheme: const TextTheme(
           button: TextStyle(
               fontSize: 16, color: Colors.black, fontWeight: FontWeight.w500)));
--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,3 +1,4 @@
 class AppStrings {
   static const String appName = 'Quotes';
+  static const String fontFamily = 'Tajawal';
 }
