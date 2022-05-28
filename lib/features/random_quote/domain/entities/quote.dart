bef8213746f58053af5fff404ea0ab382472991c
ismailelmogy <ismailelmogy0@gmail.com>
2022-05-28 02:10:29 +0200

    add quote entity

---
+++ lib/features/random_quote/domain/entities/quote.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class Quote extends Equatable {
+  final String author;
+  final int id;
+  final String content;
+  final String permalink;
+
+  const Quote(
+      {required this.author,
+      required this.id,
+      required this.content,
+      required this.permalink});
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
