97bf640c8d2f0197a6637a12af342f053659c1d0
ismailelmogy <ismailelmogy0@gmail.com>
2022-05-30 08:15:41 +0200

    add  local data source

--- lib/features/random_quote/data/datasources/random_quote_local_data_source.dart
+++ lib/features/random_quote/data/datasources/random_quote_local_data_source.dart
@@ -1,6 +1,36 @@
+import 'dart:convert';
+
+import 'package:quotes/core/error/exceptions.dart';
+import 'package:quotes/core/utils/app_strings.dart';
 import 'package:quotes/features/random_quote/data/models/quote_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class RandomQuoteLocalDataSource {
   Future<QuoteModel> getLastRandomQuote();
   Future<void> cacheQuote(QuoteModel quote);
+}
+
+class RandomQuoteLocalDataSourceImpl implements RandomQuoteLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  RandomQuoteLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<QuoteModel> getLastRandomQuote() {
+    final jsonString =
+        sharedPreferences.getString(AppStrings.cachedRandomQuote);
+    if (jsonString != null) {
+      final cacheRandomQuote =
+          Future.value(QuoteModel.fromJson(json.decode(jsonString)));
+      return cacheRandomQuote;
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheQuote(QuoteModel quote) {
+    return sharedPreferences.setString(
+        AppStrings.cachedRandomQuote, json.encode(quote));
+  }
 }
